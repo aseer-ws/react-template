aa2b5abda2814fb5835e6d1839fd80ef21ec4488
Aseer KT <aseer@wednesday.is>
2022-02-03 13:00:28 +0530

    chore: add test for undefined artistName for tunesApi

--- app/services/tests/tunesApi.test.js
+++ app/services/tests/tunesApi.test.js
@@ -3,9 +3,15 @@ import { getTunes } from '@services/tunesApi';
 import { getApiClient } from '@app/utils/apiUtils';
 
 describe('tunesApi test', () => {
-  const artistName = 'Arijit Singh';
+  let artistName;
+  let mock;
+
+  beforeAll(() => {
+    artistName = 'Author C';
+    mock = new MockAdapter(getApiClient('itunes').axiosInstance);
+  });
+
   it('should make api call to /search?term=', async () => {
-    const mock = new MockAdapter(getApiClient('itunes').axiosInstance);
     const data = {
       resultCount: 1,
       results: [{ artistName }]
@@ -14,5 +20,14 @@ describe('tunesApi test', () => {
     mock.onGet(`search?term=${artistName}`).reply(200, data);
     const res = await getTunes(artistName);
     expect(res.data).toEqual(data);
+  });
+
+  it('should fail if artistName is undefined', async () => {
+    let artistName;
+    const mock = new MockAdapter(getApiClient('itunes').axiosInstance);
+    const problem = 'SERVER_ERROR';
+    mock.onGet(`/search?term=${artistName}`).reply(500, problem);
+    const res = await getTunes(artistName);
+    expect(res.problem).toBe(problem);
   });
 });
