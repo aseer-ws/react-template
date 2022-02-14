4a53f6d69d553f3384961f3d2437eec9d0b54e14
Aseer KT <aseer@wednesday.is>
2022-02-14 14:05:21 +0530

    Revert "test: add tests for history with the latest changes"
    This reverts commit eaad4be621d6f7c7f6a176bfbccbc6fdb7042114.

--- app/utils/history.js
+++ app/utils/history.js
@@ -1,4 +1,4 @@
 import { createBrowserHistory } from 'history';
-export const baseUrl = () => process.env.PUBLIC_PATH || '/';
+export const baseUrl = () => (process.env.NODE_ENV === 'production' ? '/react-template' : '/');
 const history = createBrowserHistory({ basename: baseUrl() });
 export default history;
