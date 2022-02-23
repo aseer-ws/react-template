94a98a04d4195d2a85e6de449e06c303e582fc74
Aseer KT <aseer@wednesday.is>
2022-02-23 18:01:03 +0530

    test: TrackFormContainer tests 99%

--- app/containers/TrackFormContainer/tests/index.test.js
+++ app/containers/TrackFormContainer/tests/index.test.js
@@ -10,23 +10,10 @@ import { fireEvent, waitFor } from '@testing-library/react';
 import { renderProvider, timeout } from '@utils/testUtils';
 import { message, notification } from 'antd';
 import React from 'react';
-import {
-  INVALID_TYPE_MESSAGE,
-  ISLT2M_MESSAGE,
-  mapDispatchToProps,
-  TrackFormContainerTest as TrackFormContainer,
-  trackFormSteps
-} from '../index';
+import { mapDispatchToProps, TrackFormContainerTest as TrackFormContainer } from '../index';
 import { initialValues, trackFormContainerCreators } from '../reducer';
+import { INVALID_TYPE_MESSAGE, ISLT2M_MESSAGE, TRACK_SUBMIT_FAILED, TRACK_SUBMIT_SUCCESS } from '../trackFormDetails';
 
-const getMessageElm = (baseElement, status) => baseElement.querySelector(`.ant-message-${status}`);
-const getNotifElm = (baseElement) => baseElement.querySelector('.ant-notification-notice-content');
-
-const notificationContentClass = '.ant-notification-notice-content';
-
-const expectMessage = (baseElement, status, msg) => {
-  return expect(getMessageElm(baseElement, status)?.children[1].textContent).toBe(msg);
-};
 describe('<TrackFormContainer /> container tests', () => {
   let fillSpy;
   let resetSpy;
@@ -35,18 +22,20 @@ describe('<TrackFormContainer /> container tests', () => {
   let dummyFile;
 
   beforeAll(() => {
-    // jest.useFakeTimers();
     window.HTMLCanvasElement.prototype.getContext = function () {};
     window.URL.createObjectURL = function () {};
-    history.location.pathname = '/add-track/track';
     fillSpy = jest.fn();
     resetSpy = jest.fn();
     setFormSpy = jest.fn();
 
     routeProps = {
       history,
-      path: '/add-track/:stepTitle?'
+      path: '/add-track'
     };
+  });
+  beforeEach(() => {
+    history.location.pathname = '/add-track';
+    history.location.search = '?stepTitle=track';
   });
 
   afterEach(() => {
@@ -55,7 +44,6 @@ describe('<TrackFormContainer /> container tests', () => {
     setFormSpy?.mockReset();
     message.destroy();
     notification.destroy();
-    // jest.useRealTimers();
   });
 
   it('should render and match the snapshot', () => {
@@ -72,8 +60,8 @@ describe('<TrackFormContainer /> container tests', () => {
     expect(baseElement).toMatchSnapshot();
   });
 
-  it('should redirect first step if stepTitle params id is not provided', async () => {
-    history.location.pathname = '/add-track';
+  it('should redirect first step if stepTitle query param is not provided', async () => {
+    history.location.search = '';
     renderProvider(
       <TrackFormContainer
         formValues={initialValues}
@@ -84,7 +72,7 @@ describe('<TrackFormContainer /> container tests', () => {
       />,
       routeProps
     );
-    await waitFor(() => expect(history.location.pathname).toBe('/add-track/track'));
+    await waitFor(() => expect(history.location.search).toBe('?stepTitle=track'));
   });
 
   it('should not move the next step if clicked on next step button without filling in data', async () => {
@@ -98,10 +86,11 @@ describe('<TrackFormContainer /> container tests', () => {
       />,
       routeProps
     );
-    await waitFor(() => expect(history.location.pathname).toBe('/add-track/track'));
+    await timeout(300);
+
     const nextbtn = getByTestId('submit-btn');
     fireEvent.click(nextbtn);
-    await waitFor(() => expect(history.location.pathname).toBe('/add-track/track'));
+    await waitFor(() => expect(history.location.search).toBe('?stepTitle=track'));
   });
 
   it('should move to the next step if clicked on next step button with valid form data', async () => {
@@ -115,7 +104,9 @@ describe('<TrackFormContainer /> container tests', () => {
       />,
       routeProps
     );
-    await waitFor(() => expect(history.location.pathname).toBe('/add-track/track'));
+
+    // await waitFor(() => getByText(FORM_LOADING_TIP).not.toBeInTheDocument());
+    await timeout(300);
 
     // validate current step fields to success
     fireEvent.change(document.getElementById('trackName'), { target: { value: 'Track A' } });
@@ -123,7 +114,46 @@ describe('<TrackFormContainer /> container tests', () => {
 
     const nextbtn = getByTestId('submit-btn');
     fireEvent.click(nextbtn);
-    await waitFor(() => expect(history.location.pathname).toBe('/add-track/collection'));
+    await waitFor(() => expect(history.location.search).toBe('?stepTitle=collection'));
+  });
+
+  it('should be able to move to second step through url if first if already form fields are valid', async () => {
+    const { rerender, getByTestId } = renderProvider(
+      <TrackFormContainer
+        formValues={initialValues}
+        fillForm={fillSpy}
+        resetForm={resetSpy}
+        setFormValues={setFormSpy}
+        maxWidth={800}
+      />,
+      routeProps
+    );
+
+    await timeout(300);
+
+    // validate current step fields to success
+    fireEvent.change(document.getElementById('trackName'), { target: { value: 'Track A' } });
+    fireEvent.change(document.getElementById('trackViewUrl'), { target: { value: 'https://tracka.com' } });
+
+    const nextbtn = getByTestId('submit-btn');
+    fireEvent.click(nextbtn);
+
+    await timeout(500);
+
+    renderProvider(
+      <TrackFormContainer
+        formValues={initialValues}
+        fillForm={fillSpy}
+        resetForm={resetSpy}
+        setFormValues={setFormSpy}
+        maxWidth={800}
+      />,
+      routeProps,
+      rerender
+    );
+    await timeout(500);
+
+    expect(document.getElementById('collectionName')).toBeInTheDocument();
   });
 
   it('should fill the form and move to step 4 if clicked fill btn', async () => {
@@ -137,14 +167,17 @@ describe('<TrackFormContainer /> container tests', () => {
       />,
       routeProps
     );
+    // await waitFor(() => getByText(FORM_LOADING_TIP).toBeNull());
+    await timeout(300);
+
     const fillBtn = getByTestId('fill-btn');
     fireEvent.click(fillBtn);
-    const formItemToBeDisplayed = getByTestId(`${trackFormSteps[3][0].itemProps.name}_test`);
-    expect(formItemToBeDisplayed).toBeInTheDocument();
+    await waitFor(() => expect(history.location.search).toBe('?stepTitle=upload'));
   });
 
   it('should move to previous step if clicked on previous button', async () => {
-    history.location.pathname = '/add-track/collection';
+    history.location.search = '?stepTitle=collection';
+
     const { getByTestId } = renderProvider(
       <TrackFormContainer
         formValues={initialValues}
@@ -155,9 +188,26 @@ describe('<TrackFormContainer /> container tests', () => {
       />,
       routeProps
     );
+    // await waitFor(() => getByText(FORM_LOADING_TIP).toBeNull());
+    await timeout(300);
+
     const prevBtn = getByTestId('prev-btn');
+
     fireEvent.click(prevBtn);
-    await waitFor(() => expect(history.location.pathname).toBe('/add-track/track'));
+    await waitFor(() => expect(history.location.search).toBe('?stepTitle=track'));
+  });
+
+  it('should be able to move to second step through url if first if already form fields are valid', () => {
+    renderProvider(
+      <TrackFormContainer
+        formValues={initialValues}
+        fillForm={fillSpy}
+        resetForm={resetSpy}
+        setFormValues={setFormSpy}
+        maxWidth={800}
+      />,
+      routeProps
+    );
   });
 
   it('should reset the form back to initialValues if clicked on reset button', async () => {
@@ -171,11 +221,14 @@ describe('<TrackFormContainer /> container tests', () => {
       />,
       routeProps
     );
+    // await waitFor(() => getByText(FORM_LOADING_TIP).toBeNull());
+    await timeout(300);
+
     const fillBtn = getByTestId('fill-btn');
     fireEvent.click(fillBtn);
     const resetBtn = getByTestId('reset-btn');
     fireEvent.click(resetBtn);
-    await waitFor(() => expect(history.location.pathname).toBe('/add-track/track'));
+    await waitFor(() => expect(history.location.search).toBe('?stepTitle=track'));
     const loopField = ['trackName', 'trackViewUrl'];
     for (let key of loopField) {
       expect(document.getElementById(key).value).toEqual(String(initialValues[key]));
@@ -183,7 +236,7 @@ describe('<TrackFormContainer /> container tests', () => {
   });
 
   it('should show invalid file type message when selected file is not of type image', async () => {
-    const { baseElement, getByTestId, getByText } = renderProvider(
+    const { getByTestId, getByText, rerender } = renderProvider(
       <TrackFormContainer
         formValues={initialValues}
         fillForm={fillSpy}
@@ -193,9 +246,25 @@ describe('<TrackFormContainer /> container tests', () => {
       />,
       routeProps
     );
+    // await waitFor(() => getByText(FORM_LOADING_TIP).toBeNull());
+    await timeout(300);
+
     const fillBtn = getByTestId('fill-btn');
     fireEvent.click(fillBtn);
-    await waitFor(() => expect(history.location.pathname).toBe('/add-track/upload'));
+
+    await waitFor(() => expect(history.location.search).toBe('?stepTitle=upload'));
+    await timeout(500);
+    renderProvider(
+      <TrackFormContainer
+        formValues={initialValues}
+        fillForm={fillSpy}
+        resetForm={resetSpy}
+        setFormValues={setFormSpy}
+        maxWidth={800}
+      />,
+      routeProps,
+      rerender
+    );
 
     const uploadInput = getByTestId('uploader');
 
@@ -206,11 +275,11 @@ describe('<TrackFormContainer /> container tests', () => {
       }
     });
 
-    await waitFor(() => expectMessage(baseElement, 'error', INVALID_TYPE_MESSAGE));
+    await waitFor(() => expect(getByText(INVALID_TYPE_MESSAGE)).not.toBeNull());
   });
 
   it('should show error popup message when selected file size is greater than 2MB', async () => {
-    const { baseElement, getByTestId } = renderProvider(
+    const { getByText, getByTestId, rerender } = renderProvider(
       <TrackFormContainer
         formValues={initialValues}
         fillForm={fillSpy}
@@ -222,7 +291,20 @@ describe('<TrackFormContainer /> container tests', () => {
     );
     const fillBtn = getByTestId('fill-btn');
     fireEvent.click(fillBtn);
-    await waitFor(() => expect(history.location.pathname).toBe('/add-track/upload'));
+    await waitFor(() => expect(history.location.search).toBe('?stepTitle=upload'));
+    await timeout(500);
+
+    renderProvider(
+      <TrackFormContainer
+        formValues={initialValues}
+        fillForm={fillSpy}
+        resetForm={resetSpy}
+        setFormValues={setFormSpy}
+        maxWidth={800}
+      />,
+      routeProps,
+      rerender
+    );
 
     const uploadInput = getByTestId('uploader');
     dummyFile = new File(['somestuff'], 'camera.jpeg', { type: 'image/jpeg' });
@@ -233,37 +315,11 @@ describe('<TrackFormContainer /> container tests', () => {
       }
     });
 
-    await waitFor(() => expectMessage(baseElement, 'error', ISLT2M_MESSAGE));
+    await waitFor(() => expect(getByText(ISLT2M_MESSAGE)).not.toBeNull());
   });
 
-  // it.only('should show message for failed image upload', async () => {
-  //   const { baseElement, getByTestId } = renderProvider(
-  //     <TrackFormContainer
-  //       formValues={initialValues}
-  //       fillForm={fillSpy}
-  //       resetForm={resetSpy}
-  //       setFormValues={setFormSpy}
-  //       maxWidth={800}
-  //     />,
-  //     routeProps
-  //   );
-  //   const fillBtn = getByTestId('fill-btn');
-  //   fireEvent.click(fillBtn);
-  //   await waitFor(() => expect(history.location.pathname).toBe('/add-track/upload'));
-
-  //   const uploadInput = getByTestId('uploader');
-  //   dummyFile = new File(['somestuff2'], 'camera.jpeg', { type: 'image/jpeg' });
-  //   Object.defineProperty(dummyFile, 'size', { value: 10 });
-  //   fireEvent.change(uploadInput, {
-  //     target: {
-  //       files: [dummyFile]
-  //     }
-  //   });
-  //   await waitFor(() => expectMessage(baseElement, 'error', `${dummyFile.name} file upload failed.`));
-  // });
-
-  it('should submit values to dummyApi and gives error notification', async () => {
-    const { getByTestId, baseElement, getByText } = renderProvider(
+  it('should submit values to dummyApi and gives success notification and opens modal and resets form on closing', async () => {
+    const { getByTestId, getByText, rerender } = renderProvider(
       <TrackFormContainer
         formValues={initialValues}
         fillForm={fillSpy}
@@ -275,7 +331,67 @@ describe('<TrackFormContainer /> container tests', () => {
     );
     const fillBtn = getByTestId('fill-btn');
     fireEvent.click(fillBtn);
-    await waitFor(() => expect(history.location.pathname).toBe('/add-track/upload'));
+    await waitFor(() => expect(history.location.search).toBe('?stepTitle=upload'));
+    await timeout(500);
+
+    renderProvider(
+      <TrackFormContainer
+        formValues={initialValues}
+        fillForm={fillSpy}
+        resetForm={resetSpy}
+        setFormValues={setFormSpy}
+        maxWidth={800}
+      />,
+      routeProps,
+      rerender
+    );
+
+    const uploadInput = getByTestId('uploader');
+    dummyFile = new File(['abcde', 'sutffagain'], 'camera1.jpeg', { type: 'image/jpeg' });
+    fireEvent.change(uploadInput, {
+      target: {
+        files: [dummyFile]
+      }
+    });
+    await timeout(200);
+    await waitFor(() => expect(getByText(`${dummyFile.name} file uploaded.`)).not.toBeNull());
+
+    const submitBtn = getByTestId('submit-btn');
+    process.env.TEST_MOCK_STATUS_CODE = 200;
+    fireEvent.click(submitBtn);
+    await waitFor(() => expect(getByText(TRACK_SUBMIT_SUCCESS)).not.toBeNull());
+    await waitFor(() => expect(getByTestId('track-modal')).toBeInTheDocument());
+    fireEvent.click(getByTestId('close-modal'));
+    await waitFor(() => expect(history.location.search).toBe('?stepTitle=track'));
+  });
+
+  it('should submit values to dummyApi and gives error notification', async () => {
+    const { getByTestId, getByText, rerender } = renderProvider(
+      <TrackFormContainer
+        formValues={initialValues}
+        fillForm={fillSpy}
+        resetForm={resetSpy}
+        setFormValues={setFormSpy}
+        maxWidth={800}
+      />,
+      routeProps
+    );
+    const fillBtn = getByTestId('fill-btn');
+    fireEvent.click(fillBtn);
+    await waitFor(() => expect(history.location.search).toBe('?stepTitle=upload'));
+    await timeout(500);
+
+    renderProvider(
+      <TrackFormContainer
+        formValues={initialValues}
+        fillForm={fillSpy}
+        resetForm={resetSpy}
+        setFormValues={setFormSpy}
+        maxWidth={800}
+      />,
+      routeProps,
+      rerender
+    );
 
     const uploadInput = getByTestId('uploader');
     dummyFile = new File(['somestuff2'], 'camera.jpeg', { type: 'image/jpeg' });
@@ -284,14 +400,12 @@ describe('<TrackFormContainer /> container tests', () => {
         files: [dummyFile]
       }
     });
-    await waitFor(() => expect(getByText(INVALID_TYPE_MESSAGE)).not.toBeNull());
-
-    // await waitFor(() => expectMessage(baseElement, 'error', `${dummyFile.name} file upload failed.`));
+    await waitFor(() => expect(getByText(`${dummyFile.name} file uploaded.`)).not.toBeNull());
 
     const submitBtn = getByTestId('submit-btn');
     process.env.TEST_MOCK_STATUS_CODE = 400;
     fireEvent.click(submitBtn);
-    await waitFor(() => expect(baseElement.querySelector(notificationContentClass)).toBeInTheDocument());
+    await waitFor(() => expect(getByText(TRACK_SUBMIT_FAILED)).not.toBeNull());
   });
 
   it('should maptchDispatchToProps should dispatch action of specific type', () => {
@@ -306,69 +420,4 @@ describe('<TrackFormContainer /> container tests', () => {
     props.resetForm();
     expect(dispatchSpy).toBeCalledWith(resetForm());
   });
-
-  it('should submit values to dummyApi and gives success notification and opens modal and resets form on closing', async () => {
-    const { getByTestId, baseElement } = renderProvider(
-      <TrackFormContainer
-        formValues={initialValues}
-        fillForm={fillSpy}
-        resetForm={resetSpy}
-        setFormValues={setFormSpy}
-        maxWidth={800}
-      />,
-      routeProps
-    );
-    const fillBtn = getByTestId('fill-btn');
-    fireEvent.click(fillBtn);
-    await waitFor(() => expect(history.location.pathname).toBe('/add-track/upload'));
-
-    const uploadInput = getByTestId('uploader');
-    dummyFile = new File(['abcde', 'sutffagain'], 'camera1.jpeg', { type: 'image/jpeg' });
-    fireEvent.change(uploadInput, {
-      target: {
-        files: [dummyFile]
-      }
-    });
-    await timeout(200);
-    await waitFor(() => expectMessage(baseElement, 'error', `${dummyFile.name} file upload failed.`));
-
-    const submitBtn = getByTestId('submit-btn');
-    process.env.TEST_MOCK_STATUS_CODE = 200;
-    fireEvent.click(submitBtn);
-    await waitFor(() => expect(getNotifElm(baseElement)).toBeInTheDocument());
-    await waitFor(() => expect(getByTestId('track-modal')).toBeInTheDocument());
-    fireEvent.click(getByTestId('close-modal'));
-    await waitFor(() => expect(history.location.pathname).toBe('/add-track/track'));
-  });
-
-  // it('should submit values to dummyApi and gives error notification', async () => {
-  //   const { getByTestId, baseElement, debug } = renderProvider(
-  //     <TrackFormContainer
-  //       formValues={initialValues}
-  //       fillForm={fillSpy}
-  //       resetForm={resetSpy}
-  //       setFormValues={setFormSpy}
-  //       maxWidth={800}
-  //     />,
-  //     routeProps
-  //   );
-  //   const fillBtn = getByTestId('fill-btn');
-  //   fireEvent.click(fillBtn);
-  //   await waitFor(() => expect(history.location.pathname).toBe('/add-track/upload'));
-
-  //   const uploadInput = getByTestId('uploader');
-  //   dummyFile = new File(['somestuff2'], 'camera.jpeg', { type: 'image/jpeg' });
-  //   fireEvent.change(uploadInput, {
-  //     target: {
-  //       files: [dummyFile]
-  //     }
-  //   });
-  //   debug();
-  //   await waitFor(() => expectMessage(baseElement, 'error', `${dummyFile.name} file upload failed.`));
-
-  //   const submitBtn = getByTestId('submit-btn');
-  //   process.env.TEST_MOCK_STATUS_CODE = 400;
-  //   fireEvent.click(submitBtn);
-  //   await waitFor(() => expect(getNotifElm(baseElement)).toBeInTheDocument());
-  // });
 });
